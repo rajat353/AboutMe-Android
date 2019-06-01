69e4a245c76194bdf59339ea72733633d9dad57e
Rajat Garg <gargrajat353@gmail.com>
2019-06-01 19:23:14 +0530

    Used Log.i method to log the user's input like name and enrollment number. Applied constraint on the length of the input values.

--- app/src/main/java/com/rj/helloandroid/MainActivity.java
+++ app/src/main/java/com/rj/helloandroid/MainActivity.java
@@ -2,9 +2,19 @@ package com.rj.helloandroid;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void submitResponce(View b1)
+    {
+        EditText n1= findViewById(R.id.name);
+        EditText p1= findViewById(R.id.phone);
+        Log.i("Name", n1.getText().toString());
+        Log.i("Phone", p1.getText().toString());
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
