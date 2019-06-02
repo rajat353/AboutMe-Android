7886aedabb8ed0d7fe3a247989a29d3d34b30502
Rajat Garg <gargrajat353@gmail.com>
2019-06-02 20:28:46 +0530

    Added Toast(to show notifications) and Profile Image

--- app/src/main/java/com/rj/helloandroid/MainActivity.java
+++ app/src/main/java/com/rj/helloandroid/MainActivity.java
@@ -1,10 +1,12 @@
 package com.rj.helloandroid;
 
+        import android.app.Activity;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.util.Log;
         import android.view.View;
         import android.widget.EditText;
+        import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +14,9 @@ public class MainActivity extends AppCompatActivity {
     {
         EditText n1= findViewById(R.id.name);
         EditText p1= findViewById(R.id.phone);
-        Log.i("Name", n1.getText().toString());
-        Log.i("Phone", p1.getText().toString());
+//        Log.i("Name", n1.getText().toString());
+//        Log.i("Phone", p1.getText().toString());
+        Toast.makeText(MainActivity.this,"Your Name is " + n1.getText().toString() + "\nYour Enrollment No. is " + p1.getText().toString(),Toast.LENGTH_LONG).show();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
